8c8ca6eb3847fb66c6ce37b49056f1029614e8c3
amuyassar17 <amuyassar16@gmail.com>
2020-09-27 03:48:33 +0800

    Updates

---
+++ precache-manifest.c000128755d2a7fcafabdc111a467622.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d8279079a01fd1e99026d9b5502016",
+    "url": "/ahmadcv/index.html"
+  },
+  {
+    "revision": "d5e9c0da9e70a4c7605c",
+    "url": "/ahmadcv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2acd7789c9683aca279d",
+    "url": "/ahmadcv/static/js/2.287a0754.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ahmadcv/static/js/2.287a0754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5e9c0da9e70a4c7605c",
+    "url": "/ahmadcv/static/js/main.506c4ebc.chunk.js"
+  },
+  {
+    "revision": "1699cfdde4c93d1d71bd",
+    "url": "/ahmadcv/static/js/runtime-main.5964f0e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ahmadcv/static/media/logo.5d5d9eef.svg"
+  }
+]);
