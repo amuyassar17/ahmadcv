c47e58466455d56772e63766bce56d11c851668e
amuyassar17 <amuyassar16@gmail.com>
2021-11-13 04:40:54 +0700

    deploy

---
+++ build/precache-manifest.f21940b4a6adf6981a2ad3262402d10c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0f3eb71717e2433a677cde8cbec930",
+    "url": "/ahmadcv/index.html"
+  },
+  {
+    "revision": "b9503448801b7480ed17",
+    "url": "/ahmadcv/static/css/2.cbc5e724.chunk.css"
+  },
+  {
+    "revision": "3585d4d2ce59ccdb56ec",
+    "url": "/ahmadcv/static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "b9503448801b7480ed17",
+    "url": "/ahmadcv/static/js/2.6d4a822b.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/ahmadcv/static/js/2.6d4a822b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3585d4d2ce59ccdb56ec",
+    "url": "/ahmadcv/static/js/main.dbfb4f7b.chunk.js"
+  },
+  {
+    "revision": "1699cfdde4c93d1d71bd",
+    "url": "/ahmadcv/static/js/runtime-main.5964f0e5.js"
+  },
+  {
+    "revision": "8f65ed4eed2408b76e81fd7ec46551d2",
+    "url": "/ahmadcv/static/media/ahmad.8f65ed4e.jpg"
+  }
+]);
